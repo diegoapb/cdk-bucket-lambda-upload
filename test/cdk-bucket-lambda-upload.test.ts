c6ba04aee85cba24f7c86390a6d737dbb1589d8d
Diego Parra <parra.diego@correounivalle.edu.co>
2022-04-15 22:05:09 -0500

    Initial commit

---
+++ test/cdk-bucket-lambda-upload.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkBucketLambdaUpload from '../lib/cdk-bucket-lambda-upload-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-bucket-lambda-upload-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkBucketLambdaUpload.CdkBucketLambdaUploadStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
