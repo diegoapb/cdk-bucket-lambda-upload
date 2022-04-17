f6e481cbc4b0ac2eccda11955c05ada869e20469
Diego Parra <parra.diego@correounivalle.edu.co>
2022-04-17 13:35:22 -0500

    ADD stack infrastructure

--- lib/cdk-bucket-lambda-upload-stack.ts
+++ lib/cdk-bucket-lambda-upload-stack.ts
@@ -1,16 +1,31 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { lambda_layer_awscli, Stack, StackProps } from "aws-cdk-lib";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
+import { join } from "path";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkBucketLambdaUploadStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        const s3DemoBucket = new Bucket(this, "s3_demo_bucket", {
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkBucketLambdaUploadQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        //create node lambda to upload file to s3
+        const uploadLambda = new NodejsFunction(this, "upload-lambda", {
+            entry: join(
+                __dirname,
+                "..",
+                "services",
+                "upload-lambda.ts"),
+            handler: "handler",
+            environment: {
+                S3_BUCKET_NAME: s3DemoBucket.bucketName,
+            },
+        });
+
+        s3DemoBucket.grantReadWrite(uploadLambda);
+
+    }
 }
